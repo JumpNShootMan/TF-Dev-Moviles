001f6eb0d4520cb95be10f391d60eaf1b2454daa
RenzoPL23 <renperez157@gmail.com>
2021-06-17 21:57:17 -0500

    assets

--- js/states/Preload.js
+++ js/states/Preload.js
@@ -12,6 +12,7 @@ Preload.prototype = {
 		// this.game.load.image("floor", "assets/images/floor.png");
 		// this.game.load.image("player_dead", "assets/images/player_dead.png");
 		// this.game.load.spritesheet("player_spritesheet", "assets/images/player_spritesheet.png",51,67,5);
+		// this.game.load.spritesheet("player_spritesheet", "assets/images/Biker_run.png",48,48,5);
 		// this.game.load.image("water", "assets/images/water.png");
 		// this.game.load.audio("coin",['assets/audio/coin.mp3','assets/audio/coin.ogg']);
 	},
