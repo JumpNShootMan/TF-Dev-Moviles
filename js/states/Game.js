0a2df6a5dfa57180f9c1fe2b2f5063510a8f764a
RenzoPL23 <renperez157@gmail.com>
2021-06-17 22:19:36 -0500

    player and labels

--- js/states/Game.js
+++ js/states/Game.js
@@ -1,7 +1,7 @@
 Game = function (game) {}
 
 Game.prototype = {
-	init: function (param1, param2, param3) {
+	init: function (param1) {
 		// console.log(param1);
 		// console.log(param2);
 		// console.log(param3);
@@ -11,17 +11,18 @@ Game.prototype = {
 		// this.game.physics.arcade.gravity.y = 1000;
 		// this.maxJumpDistance = 120;
 		// this.cursors = this.game.input.keyboard.createCursorKeys();
-		// this.myCoins = 0;
+		this.points = 0;
+		this.life = param1;
 	},
 	create: function () {
 		// this.background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'background');
 		// this.background.tileScale.y = 2;
 		// this.background.autoScroll(this.levelSpeed / 6, 0);
 
-		// this.player = this.game.add.sprite(50, 140, 'player_spritesheet');
-		// this.player.anchor.setTo(0.5);
-		// this.player.animations.add('running', [0, 1, 2, 3, 2, 1], 15, true);
-		// this.player.animations.play("running");
+		this.player = this.game.add.sprite(50, 240, 'player_run');
+		this.player.anchor.setTo(0.5);
+		this.player.animations.add('running', [0, 1, 2, 3, 4, 5, 4, 3, 2, 1], 15, true);
+		this.player.animations.play("running");
 		// this.game.physics.arcade.enable(this.player);
 
 		// // this.player.body.collideWorldBounds = true;
@@ -35,14 +36,15 @@ Game.prototype = {
 
 		// this.currentPlatform = new Platform(this.game, this.floorPool, 11, 0, 200, -this.levelSpeed, this.coinsPool);
 		// this.platformPool.add(this.currentPlatform);
-		// let style = {
-		// 	font: '30px Arial',
-		// 	fill: "#fff"
-		// };
-		// this.coinCountLabel = this.game.add.text(10, 20, '0', style)
+		let style = {
+			font: '20px Arial',
+			fill: "#fff"
+		};
+		this.pointsLabel = this.game.add.text(10, 20,'Points: '+ this.points, style)
+		this.lifeLabel = this.game.add.text( this.game.width-100, 20,'Life: '+ this.life, style)
 		// this.loadLevel();
 	},
-	
+
 	update: function () {
 
 	},
--- js/states/Preload.js
+++ js/states/Preload.js
@@ -3,20 +3,20 @@ Preload = function (game) {}
 
 Preload.prototype = {
 	preload: function () {
-		// this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		// this.game.scale.pageAlignHorizontally = true;
-		// this.game.scale.pageAlignVertically = true;
+		this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.game.scale.pageAlignHorizontally = true;
+		this.game.scale.pageAlignVertically = true;
 
 		// this.game.load.image("background", "assets/images/background.png");
 		// this.game.load.image("coin", "assets/images/coin.png");
 		// this.game.load.image("floor", "assets/images/floor.png");
 		// this.game.load.image("player_dead", "assets/images/player_dead.png");
 		// this.game.load.spritesheet("player_spritesheet", "assets/images/player_spritesheet.png",51,67,5);
-		// this.game.load.spritesheet("player_spritesheet", "assets/images/Biker_run.png",48,48,5);
+		this.game.load.spritesheet("player_run", "assets/images/Biker_run.png",48,48,6);
 		// this.game.load.image("water", "assets/images/water.png");
 		// this.game.load.audio("coin",['assets/audio/coin.mp3','assets/audio/coin.ogg']);
 	},
 	create: function () {
-		// this.game.state.start("Game",true,false,1,'modo pana',true);//limpiar mundo, limpiar cache
+		this.game.state.start("Game",true,false,3);//limpiar mundo, limpiar cache
 	}
 }
